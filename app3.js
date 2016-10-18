86b5a0728e248799d8e80d6cb239d1ebc35fd53a
j54311543 <j54311543@gmail.com>
2016-10-19 01:11:35 +0800

    123

---
+++ app3.js
@@ -0,0 +1,7 @@
+http://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_click
+
+$(document).ready(function(){
+    $("font").click(function(){
+            $(this).hide();
+                });
+                });
