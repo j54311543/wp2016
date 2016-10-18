65bcff141c8892113f50bec50d0b1e52c89aa6a7
j54311543 <j54311543@gmail.com>
2016-10-18 23:09:54 +0800

    commit note

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-<botton type="button" onclick="document.getElementById('context').innerHTML = ''">Click Me!</button>
+<botton type="button" onclick="document.getElementById('context').innerHTML = 'HI, Nice to make friends with you.'">Make friends with Me!</button>
 
