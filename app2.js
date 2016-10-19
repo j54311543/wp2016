a0233f68af7b14166ca0734058308603d123e7f1
j54311543 <j54311543@gmail.com>
2016-10-19 18:34:52 +0800

    123

--- app2.js
+++ app2.js
@@ -1,3 +1,3 @@
 http://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_inner_html
-<botton type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Click Me!</button>
+document.getElementById('demo').innerHTML = 'Hello JavaScript!'
 
